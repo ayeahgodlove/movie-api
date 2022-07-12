a3671bf6e9fa4d28660c5ad367ef143efd98f1d2
ayeahgodlove <ayeahgodlove5@gmail.com>
2022-07-12 15:02:54 +0100

    wip(server.js): start api development

---
+++ server.js
@@ -0,0 +1,6 @@
+
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const connectDB = require('./config/db.config');
+const PORT = process.env.PORT;
