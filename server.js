314b0b5eb03a20d48348e3bcd66ad27f29d775a1
ayeahgodlove <ayeahgodlove5@gmail.com>
2022-07-12 16:50:24 +0100

    feat: set up morgan middle ware for logging

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
-
-const express = require('express');
-const colors = require('colors');
-const dotenv = require('dotenv').config();
-const connectDB = require('./config/db.config');
+const express = require("express");
+const morgan = require("morgan");
+const fs = require("fs"); // import built in node modules fs
+const path = require("path"); // and path
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db.config");
 const PORT = process.env.PORT;
 
+const app = express();
+
+// create a write stream (in append mode)
+// a ‘log.txt’ file is created in root directory
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
+  flags: "a",
+});
+app.use(morgan("combined", { stream: accessLogStream }));
+
+// call database connection
 connectDB();
+
+// app routes
+app.get("/documentation", (req, res) => {
+  res.sendFile("public/index.html", { root: __dirname });
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my app!");
+});
+
+// listen on port
+app.listen(PORT, () => {
+  console.log(`Your app is listening on port ${PORT}.`);
+});
